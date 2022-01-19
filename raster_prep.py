9ff382f137badbd71847f0256e7006b420bedeef
malle <johanna.malle@slf.ch>
2022-01-19 18:42:51 +0100

    made script prettier/updated additional information and improved readability

--- raster_prep.py
+++ raster_prep.py
@@ -1,46 +1,60 @@
-import subprocess
+# -*- coding: utf-8 -*-
+"""
+Calculate DTM, DSM, CHM from CH-wide datasets for a given extent
+Created on 17.01.22 17:37
+@author: malle
+"""
+
 import os
 import csv
 from osgeo import gdal
 from pathlib import Path
 import shutil
+import subprocess
 
-os.environ['PROJ_LIB'] = '/home/malle/miniconda3/envs/SetUp_sites/share/proj' #somehow without this it can't find the proj.db file...
+# somehow without this it can't find the proj.db file...
+os.environ['PROJ_LIB'] = '/home/malle/miniconda3/envs/SetUp_sites/share/proj'
 
-#define paths etc.
+# define paths etc.
 wrk_dir = '/home/malle/slfhome/Postdoc2/experiment_sites'
-sites  = os.listdir(wrk_dir)
+sites = os.listdir(wrk_dir)
 pycrown_dir = '/home/malle/pycrown/experiment_sites'
 
-#swiss wide chm & dtm can be loaded outside of loop:
+# swiss-wide chm & dtm can be loaded outside of loop:
+# swiss-wide CHM:
 chm_ch = '/media/malle/LaCie/Canopy_height_model/VHM/VHM_2021-08-12.tif'
-dtm_ch = '/media/malle/LaCie/Canopy_height_model/swissALTI3D_5M_CHLV95_LN02_2020.tif'
-
 in_chm = gdal.Open(chm_ch)
 in_band_chm = in_chm.GetRasterBand(1)  # chm only has 1 band
 in_gt_chm = in_chm.GetGeoTransform()  # get coordinates etc.
-inv_gt_chm = gdal.InvGeoTransform(in_gt_chm)
+inv_gt_chm = gdal.InvGeoTransform(in_gt_chm)  # needed to convert real-world coord. to image coord.
 if inv_gt_chm is None:
     raise RuntimeError('Inverse geotransform failed')
 
+# swiss-wide DTM:
+dtm_ch = '/media/malle/LaCie/Canopy_height_model/swissALTI3D_5M_CHLV95_LN02_2020.tif'
 in_dtm = gdal.Open(dtm_ch)
-in_band_dtm = in_dtm.GetRasterBand(1)  #chm only has 1 band
-in_gt_dtm = in_dtm.GetGeoTransform() #get coordinates etc.
+in_band_dtm = in_dtm.GetRasterBand(1)  # chm only has 1 band
+in_gt_dtm = in_dtm.GetGeoTransform()  # get coordinates etc.
 inv_gt_dtm = gdal.InvGeoTransform(in_gt_dtm)
 if inv_gt_dtm is None:
     raise RuntimeError('Inverse geotransform failed')
 
+# needed to create TIFFs later:
+gtiff_driver = gdal.GetDriverByName('GTiff')
+
+# all BD sites:
 for site in sites:
     site = os.path.basename(site)
-    chm_cut = os.path.join(wrk_dir, site,"CHM.tif")
-    dtm_cut_orig = os.path.join(wrk_dir, site, site + "_dtm_cut_orig.tif")
-    dtm_cut = os.path.join(wrk_dir, site,"DTM.tif")
-    dsm_cut = os.path.join(wrk_dir, site,"DSM.tif")
 
-    out_pycrown = os.path.join(pycrown_dir, site,'data')
+    # def. all output files/locations:
+    chm_cut = os.path.join(wrk_dir, site, "CHM.tif")
+    dtm_cut_orig = os.path.join(wrk_dir, site, site + "_dtm_cut_orig.tif")
+    dtm_cut = os.path.join(wrk_dir, site, "DTM.tif")
+    dsm_cut = os.path.join(wrk_dir, site, "DSM.tif")
+    out_pycrown = os.path.join(pycrown_dir, site, 'data')
     Path(out_pycrown).mkdir(parents=True, exist_ok=True)
 
-    #read coordinate file
+    # read coordinate file
     coord_file = os.path.join(wrk_dir, site, "coord.csv")
     xys = []
     with open(coord_file) as fp:
@@ -49,21 +63,19 @@ for site in sites:
         for row in reader:
             xys.append([float(n) for n in row[:2]])
     upper_left, lower_right = xys[0], xys[1]
-    ul_x, ul_y = upper_left[0],upper_left[1]
-    lr_x, lr_y = lower_right[0],lower_right[1]
+    ul_x, ul_y = upper_left[0], upper_left[1]
+    lr_x, lr_y = lower_right[0], lower_right[1]
 
+    # 1) CHM:
     offsets_ul = gdal.ApplyGeoTransform(inv_gt_chm, ul_x, ul_y)
     offsets_lr = gdal.ApplyGeoTransform(inv_gt_chm, lr_x, lr_y)
     off_ulx, off_uly = map(int, offsets_ul)
     off_lrx, off_lry = map(int, offsets_lr)
-
     # number rows/columns of new extent
     rows_chm = off_lry - off_uly
     columns_chm = off_lrx - off_ulx
-
-    gtiff_driver = gdal.GetDriverByName('GTiff')
-    out_ds = gtiff_driver.Create(chm_cut, columns_chm,rows_chm,1,gdal.GDT_Float32)
-
+    # create new clipped CHM file:
+    out_ds = gtiff_driver.Create(chm_cut, columns_chm, rows_chm, 1, gdal.GDT_Float32)
     out_ds.SetProjection(in_chm.GetProjection())
     subset_ulx, subset_uly = gdal.ApplyGeoTransform(in_gt_chm, off_ulx, off_uly)
     out_gt = list(in_gt_chm)
@@ -74,46 +86,46 @@ for site in sites:
     data_chm = in_band_chm.ReadAsArray(off_ulx, off_uly, columns_chm, rows_chm)
     out_band.WriteArray(data_chm)
     out_band.FlushCache()   # write to disk
-    out_band = None  # close properly!
-    out_ds = None # close properly!
+    out_band = None  # close properly! (important!!)
+    out_ds = None  # close properly! (important!!)
 
-    # now same for dtm
+    # 2) DTM:
     offsets_ul = gdal.ApplyGeoTransform(inv_gt_dtm, ul_x, ul_y)
     offsets_lr = gdal.ApplyGeoTransform(inv_gt_dtm, lr_x, lr_y)
     off_ulx, off_uly = map(int, offsets_ul)
     off_lrx, off_lry = map(int, offsets_lr)
-
     rows = off_lry - off_uly
     columns = off_lrx - off_ulx
-    out_ds_dtm = gtiff_driver.Create(dtm_cut_orig, columns, rows, 1,gdal.GDT_Float32)
+    # create new clipped DTM file:
+    out_ds_dtm = gtiff_driver.Create(dtm_cut_orig, columns, rows, 1, gdal.GDT_Float32)
     out_ds_dtm.SetProjection(in_dtm.GetProjection())
     subset_ulx, subset_uly = gdal.ApplyGeoTransform(in_gt_dtm, off_ulx, off_uly)
     out_gt = list(in_gt_dtm)
     out_gt[0] = subset_ulx
     out_gt[3] = subset_uly
     out_ds_dtm.SetGeoTransform(out_gt)
-
-    out_band = out_ds_dtm.GetRasterBand(1)
+    out_band_dtm = out_ds_dtm.GetRasterBand(1)
     data_dtm = in_band_dtm.ReadAsArray(off_ulx, off_uly, columns, rows)
-    out_band.WriteArray(data_dtm)
-    out_band.FlushCache()   # write to disk
-    out_band = None  # close properly!
-    out_ds_dtm = None  # close properly!
+    out_band_dtm.WriteArray(data_dtm)
+    out_band_dtm.FlushCache()   # write to disk
+    out_band_dtm = None  # close properly! (important!!)
+    out_ds_dtm = None  # close properly! (important!!)
 
-    #now also resample dtm to higher res! - use gdal lib - need to make sure file does not exist yet!!
+    # 3) DTM resampled to higher CHM res. :
     if os.path.exists(dtm_cut):
         os.remove(dtm_cut)
-
-    args = ['gdalwarp','-tr','1.0','1.0','-r','bilinear',dtm_cut_orig,dtm_cut]
+    # use gdal lib (file can't exist yet!)
+    args = ['gdalwarp', '-tr', '1.0', '1.0', '-r', 'bilinear', dtm_cut_orig, dtm_cut]
     result = subprocess.call(args)
 
+    # 4) DSM creation by adding DTM+CHM :
     if os.path.exists(dsm_cut):
         os.remove(dsm_cut)
-
-    args1 = ['gdal_calc.py','-A',dtm_cut,'-B',chm_cut,'--outfile',dsm_cut,'--calc','A+B']
+    # use gdal lib (file can't exist yet!)
+    args1 = ['gdal_calc.py', '-A', dtm_cut, '-B', chm_cut, '--outfile', dsm_cut, '--calc', 'A+B']
     result1 = subprocess.call(args1)
 
-    # now copy chm,dtm,dsm pycrown data directory
-    shutil.copy(dsm_cut,out_pycrown)
+    # 5) now copy chm,dtm,dsm pycrown data directory
+    shutil.copy(dsm_cut, out_pycrown)
     shutil.copy(dtm_cut, out_pycrown)
     shutil.copy(chm_cut, out_pycrown)
---
+++ run_pycrown.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Desc: 
+Created on 19.01.22 17:37
+@author: malle
+"""
+
+from datetime import datetime
+
+from pycrown import PyCrown
+
+if __name__ == '__main__':
+
+    TSTART = datetime.now()
+
+    F_CHM = '/home/malle/pycrown/experiment_sites/laret_test/data/CHM.tif'
+    F_DTM = '/home/malle/pycrown/experiment_sites/laret_test/data/DTM.tif'
+    F_DSM = '/home/malle/pycrown/experiment_sites/laret_test/data/DSM.tif'
+    F_LAS = ''
+
+    PC = PyCrown(F_CHM, F_DTM, F_DSM, outpath='/home/malle/pycrown/experiment_sites/laret_test/result')
+
+    # Cut off edges
+    # PC.clip_data_to_bbox((1802200, 1802400, 5467250, 5467450))
+
+    # Smooth CHM with 5m median filter
+    PC.filter_chm(3, ws_in_pixels=True)
+
+    # Tree Detection with local maximum filter
+    PC.tree_detection(PC.chm, ws=3, ws_in_pixels=True, hmin=3.)
+
+    # Clip trees to bounding box (no trees on image edge)
+    # original extent: 1802140, 1802418, 5467295, 5467490
+    # PC.clip_trees_to_bbox(bbox=(1802150, 1802408, 5467305, 5467480))
+    # PC.clip_trees_to_bbox(bbox=(1802160, 1802400, 5467315, 5467470))
+    PC.clip_trees_to_bbox(inbuf=0.5)  # inward buffer of 11 metre
+
+    # Crown Delineation
+    PC.crown_delineation(algorithm='dalponte_numba', th_tree=3.,
+                         th_seed=0.7, th_crown=0.55, max_crown=20.)
+
+    # Correct tree tops on steep terrain
+    PC.correct_tree_tops()
+
+    # Calculate tree height and elevation
+    PC.get_tree_height_elevation(loc='top')
+    PC.get_tree_height_elevation(loc='top_cor')
+
+    # Screen small trees
+    PC.screen_small_trees(hmin=2., loc='top')
+
+    # Convert raster crowns to polygons
+    PC.crowns_to_polys_raster()
+   # PC.crowns_to_polys_smooth(store_las=False)
+
+    # Check that all geometries are valid
+    PC.quality_control()
+
+    # Export results
+    PC.export_raster(PC.chm, PC.outpath / 'chm.tif', 'CHM')
+    PC.export_tree_locations(loc='top')
+    PC.export_tree_locations(loc='top_cor')
+    PC.export_tree_crowns(crowntype='crown_poly_raster')
+   # PC.export_tree_crowns(crowntype='crown_poly_smooth')
+
+
+    TEND = datetime.now()
+
+    print(f"Number of trees detected: {len(PC.trees)}")
+    print(f'Processing time: {TEND-TSTART} [HH:MM:SS]')
