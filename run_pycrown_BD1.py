9b470721ba4fecaf14f802aa01eb7753d02c8821
malle <johanna.malle@slf.ch>
2022-06-10 17:38:25 +0200

    Updated manual cutting algorithm... no more convex hull

--- run_pycrown_BD1.py
+++ run_pycrown_BD1.py
@@ -14,31 +14,33 @@ if __name__ == '__main__':
 
     TSTART = datetime.now()
 
-    F_CHM = '/home/malle/pycrown/experiment_sites/BD1/data/CHM.tif'
-    F_DTM = '/home/malle/pycrown/experiment_sites/BD1/data/DTM.tif'
-    F_DSM = '/home/malle/pycrown/experiment_sites/BD1/data/DSM.tif'
+    site = 'BDM_1'
+
+    F_CHM = '/home/malle/pycrown/experiment_sites_select/'+site+'/data/CHM.tif'
+    F_DTM = '/home/malle/pycrown/experiment_sites_select/'+site+'/data/DTM.tif'
+    F_DSM = '/home/malle/pycrown/experiment_sites_select/'+site+'/data/DSM.tif'
     F_LAS = ''
 
-    PC = PyCrown(F_CHM, F_DTM, F_DSM, outpath='/home/malle/pycrown/experiment_sites/BD1/result/dalponteCIRC_numba_10Mrad_ws4_chm4_thseed02')
+    PC = PyCrown(F_CHM, F_DTM, F_DSM, outpath='/home/malle/pycrown/experiment_sites_select/BDM_3/result/dalponteCIRC_numba_12mrad_ws3_chm3_thseed01_thcrown01')
 
     # Cut off edges
     # PC.clip_data_to_bbox((1802200, 1802400, 5467250, 5467450))
 
     # Smooth CHM with 5m median filter
-    PC.filter_chm(4, ws_in_pixels=True)
+    PC.filter_chm(3, ws_in_pixels=True)
 
     # Tree Detection with local maximum filter
-    PC.tree_detection(PC.chm, ws=4, ws_in_pixels=True, hmin=3.)
+    PC.tree_detection(PC.chm, ws=3, ws_in_pixels=True, hmin=1.5)
 
     # Clip trees to bounding box (no trees on image edge)
     # original extent: 1802140, 1802418, 5467295, 5467490
     # PC.clip_trees_to_bbox(bbox=(1802150, 1802408, 5467305, 5467480))
     # PC.clip_trees_to_bbox(bbox=(1802160, 1802400, 5467315, 5467470))
-    PC.clip_trees_to_bbox(inbuf=0.5)  # inward buffer of 11 metre
+    # PC.clip_trees_to_bbox(inbuf=0.1)  # inward buffer of 11 metre
 
     # Crown Delineation
-    PC.crown_delineation(algorithm='dalponteCIRC_numba', th_tree=3.,
-                         th_seed=0.2, th_crown=0.55, max_crown=10.)
+    PC.crown_delineation(algorithm='dalponteCIRC_numba', th_tree=1.5,
+                         th_seed=0.1, th_crown=0.1, max_crown=12.)
 
     # Correct tree tops on steep terrain
     PC.correct_tree_tops()
@@ -48,7 +50,7 @@ if __name__ == '__main__':
     PC.get_tree_height_elevation(loc='top_cor')
 
     # Screen small trees
-    PC.screen_small_trees(hmin=2., loc='top')
+    PC.screen_small_trees(hmin=1.5, loc='top')
 
     # Convert raster crowns to polygons
     PC.crowns_to_polys_raster()
