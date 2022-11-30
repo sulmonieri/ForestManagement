fd3325891d717ce392d2c9c1d673d6e35b3618ee
sulmonieri <ericsulmoni@hotmail.com>
2022-11-30 09:55:20 +0100

    Update run_pycrown.py

--- run_pycrown.py
+++ run_pycrown.py
@@ -13,12 +13,12 @@ if __name__ == '__main__':
 
     TSTART = datetime.now()
 
-    F_CHM = '/home/malle/pycrown/experiment_sites/laret_test/data/CHM.tif'
-    F_DTM = '/home/malle/pycrown/experiment_sites/laret_test/data/DTM.tif'
-    F_DSM = '/home/malle/pycrown/experiment_sites/laret_test/data/DSM.tif'
+    F_CHM = '/home/vagrant/ForestManagement/aoi/adapt_chm/CHM_aoi.tif'
+    F_DTM = '/home/vagrant/ForestManagement/aoi/adapt_chm/DTM_aoi.tif'
+    F_DSM = '/home/vagrant/ForestManagement/aoi/adapt_chm/DSM_aoi.tif'
     F_LAS = ''
 
-    PC = PyCrown(F_CHM, F_DTM, F_DSM, outpath='/home/malle/pycrown/experiment_sites/laret_test/lme_tmax')
+    PC = PyCrown(F_CHM, F_DTM, F_DSM, outpath='/home/vagrant/ForestManagement/aoi/adapt_chm/results/')
 
     # Cut off edges
     # PC.clip_data_to_bbox((1802200, 1802400, 5467250, 5467450))
@@ -27,17 +27,17 @@ if __name__ == '__main__':
     PC.filter_chm(3, ws_in_pixels=True)
 
     # Tree Detection with local maximum filter
-    PC.tree_detection(PC.chm, ws=3, ws_in_pixels=True, hmin=3.)
+    PC.tree_detection(PC.chm, ws=3, ws_in_pixels=True, hmin=1.5)
 
     # Clip trees to bounding box (no trees on image edge)
     # original extent: 1802140, 1802418, 5467295, 5467490
     # PC.clip_trees_to_bbox(bbox=(1802150, 1802408, 5467305, 5467480))
     # PC.clip_trees_to_bbox(bbox=(1802160, 1802400, 5467315, 5467470))
-    PC.clip_trees_to_bbox(inbuf=0.5)  # inward buffer of 11 metre
+    # PC.clip_trees_to_bbox(inbuf=0.1)  # inward buffer of 11 metre
 
     # Crown Delineation
-    PC.crown_delineation(algorithm='dalponte_numba', th_tree=3.,
-                         th_seed=0.7, th_crown=0.55, max_crown=20.)
+    PC.crown_delineation(algorithm='dalponteCIRC_numba', th_tree=1.5,
+                         th_seed=0.1, th_crown=0.1, max_crown=12.)
 
     # Correct tree tops on steep terrain
     PC.correct_tree_tops()
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     PC.get_tree_height_elevation(loc='top_cor')
 
     # Screen small trees
-    PC.screen_small_trees(hmin=2., loc='top')
+    PC.screen_small_trees(hmin=1.5, loc='top')
 
     # Convert raster crowns to polygons
     PC.crowns_to_polys_raster()
