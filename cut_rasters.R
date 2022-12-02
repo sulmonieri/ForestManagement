a3ab28bdf0bff80f848d61f8449f4b3c92498c84
sulmonieri <ericsulmoni@hotmail.com>
2022-12-02 15:23:10 +0100

    Update cut_rasters.R

--- cut_rasters.R
+++ cut_rasters.R
@@ -21,6 +21,7 @@ tpi <- rast('nationwide/TPI.tif')
 slope <- rast('nationwide/slope.tif')
 aspect_n <- rast('nationwide/aspect_n.tif')
 skyview <- rast('nationwide/skyview.tif')
+potrad <- rast('nationwide/POTRAD.tif')
 
 ## read shapfile
 shp <- st_read('shp/BDM1.shp')
@@ -34,6 +35,7 @@ slope_crop <- crop(slope,shp,overwrite=TRUE,filename='aoi/slope_aoi.tif',gdal=c(
 aspect_n_crop <- crop(aspect_n,shp,overwrite=TRUE,filename='aoi/aspect_n_aoi.tif',gdal=c('COMPRESS=DEFLATE','PREDICTOR=3'))
 skyview_crop <- crop(skyview,shp,overwrite=TRUE,filename='aoi/skyview_aoi.tif',gdal=c('COMPRESS=DEFLATE','PREDICTOR=3'))
 fmask_crop <- crop(fmask,shp,overwrite=TRUE,filename='aoi/adapt_chm/Forest_mask_10m.tif',gdal=c('COMPRESS=DEFLATE','PREDICTOR=3'))
+potrad_crop <- crop(potrad,shp,overwrite=TRUE,filename='aoi/potrad_aoi.tif',gdal=c('COMPRESS=DEFLATE','PREDICTOR=2'))
 
 
 ## DTM needs to be disaggregated to 1m (VHM resolution)
